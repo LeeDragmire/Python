d9b5c7c78e61a24c908061b58d20a4bab49ceaa6
Colton Adams <adamscolton64@gmail.com>
2023-10-06 21:21:31 -0500

    it's a bit better now

--- physics.py
+++ physics.py
@@ -7,32 +7,70 @@ acceleration = 0
 #sig figs are gonna suck balls
 
 def input_Vals():
+    distance = input("Enter distance Value (u if unknown):  ")
+    i_velocity = input("Enter Inital Velocity Value (u if unknown): ")
+    f_velocity = input("Enter Final Velocity Value (u if unknown): ")
+    time = input("Enter time Value (u if unknown): ")
+    acceleration = input("Enter acceleration Value (u if unknown): ")
+
     try:
-        distance = float(input("Enter distance Value (0 if unknown):  "))
-        i_velocity = float(input("Enter Inital Velocity Value (0 if unknown): "))
-        f_velocity = float(input("Enter Final Velocity Value (0 if unknown): "))
-        time = float(input("Enter time Value (0 if unknown): "))
-        acceleration = float(input("Enter acceleration Value (0 if unknown): "))
-    except:
-        print("An error has occured, try again \n\n\n")
+        if distance.lower() == "u":
+            distance = distance.lower()
+        else:
+            float(distance)
+
+        if i_velocity.lower() == "u":
+            i_velocity = i_velocity.lower()
+        else:
+            float(i_velocity)
+
+        if f_velocity.lower() == "u":
+            f_velocity = f_velocity.lower()
+        else:
+            float(f_velocity)
+
+        if time.lower() == "u":
+            time = time.lower()
+        else:
+            float(time)
+
+        if acceleration.lower() == "u":
+            acceleration = acceleration.lower()
+        else:
+            float(acceleration)
+    except ValueError:
+        print("\nThere was a problem try again (letters can't be numbers)\n\n")
         input_Vals()
 
-    check = "Distance is {} \nInitial Velocity is {} \nFinal Velocity is {} \nTime is {} \nAcceleration is {}"
-    if distance == 0.0:
-        distance = "..."
-    if i_velocity == 0.0:
-        i_velocity = "..."
-    if f_velocity == 0.0:
-        f_velocity = "..."
-    if time == 0.0:
-        time = "..."
-    if acceleration == 0.0:
-        acceleration = "..."
+
+    check = "\nDistance is {} \nInitial Velocity is {} \nFinal Velocity is {} \nTime is {} \nAcceleration is {}"
+
     print(check.format(distance, i_velocity, f_velocity, time, acceleration))
-    checkInput = input("Are these correct? (y/n): ")
+
+    checkInput = input("\nAre these correct? (y/n): ")
     if checkInput.lower == "n":
         input_Vals()
+    
+    calc_All(distance, i_velocity, f_velocity, time, acceleration)
 
-        
+def calc_All(d, i_v, f_v, t, a):
+    if d == "u":
+        if calc_Distance(i_v, f_v, t, a) == None:
+            print("Not enough information known")
+        else:
+            print(calc_Distance(i_v, f_v, t, a))
+
+def calc_Distance(i_v, f_v, t, a):
+    if i_v != "u" and f_v != "u" and time != "u":
+        return .5 * (float(i_v) + float(f_v)) * t
+    elif i_v != "u" and time != "u" and a != "u":
+        return float(i_v) * float(t) + .5 * float(a) * float(t) ** 2
+    elif i_v != "u" and f_v != "u" and a != "u":
+        return (float(f_v) ** 2 - float(i_v) ** 2) / (2 * float(a))
+    else:
+        return None
+    
+def calc_Initial_Velocity(d, f_v, t, a):
+    None
 
 input_Vals()
